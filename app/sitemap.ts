56e2d154bdedf3ac38f4329915ac917bf89cd692
abdulbasit-025 <abdulbasit.alpha25@gmail.com>
2026-09-08 02:54:29 +0500

    Update sitemap metadata

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl =
-    process.env.NEXT_PUBLIC_SITE_URL || "https://archiveme.vercel.app";
+  const baseUrl = process.env.SITE_URL || "https://archiveme.vercel.app";
 
   return [
     {
